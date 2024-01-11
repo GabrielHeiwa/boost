c053ce52039448625226b43bd1981a17eedada63
gabrielheiwa <gabriel.paz@beansoftwares.com.br>
2024-01-11 11:34:39 -0300

    fix: close browser

--- core/src/config/multer.ts
+++ core/src/config/multer.ts
@@ -2,9 +2,12 @@
 import multer from "multer";
 import path from "node:path";
 
+const tmpFolder = path.resolve(__dirname, "..", "..", "tmp");
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'tmp/');
+        console.log(tmpFolder)
+        cb(null, tmpFolder);
     },
     filename: function (req, file, cb) {
         const fileExtension = path.extname(file.originalname);
--- core/src/useCases/generatePDF.ts
+++ core/src/useCases/generatePDF.ts
@@ -2,13 +2,15 @@
 import puppeteer from "puppeteer";
 import { randomUUID } from "crypto";
 import path from "path";
-import fs from "node:fs";
 
 class generatePdfUseCase {
     public async execute(htmlFilename: string) {
         try {
 
-            const browser = await puppeteer.launch({ headless: "new" });
+            const browser = await puppeteer.launch({ 
+                headless: process.env.ENVIRONMENT === "dev" ? false : "new",
+                args: process.env.ENVIRONMENT === "dev" ? [] : ["--no-sandbox", "--disable-setuid-sandbox"]
+            });
 
             const page = await browser.newPage();
             const url = "http://localhost:" + process.env.PORT + "/html/" + htmlFilename;
@@ -18,6 +20,8 @@ class generatePdfUseCase {
             const pdfFilename = randomUUID() + ".pdf";
             const pdfPath = path.resolve(process.cwd(), "pdf", pdfFilename);
             await page.pdf({ path: pdfPath });
+
+            await browser.close();
 
             return pdfPath;
 
